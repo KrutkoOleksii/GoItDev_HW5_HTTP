74cef69e84d5023ad968efe26e761f7a0feeaca8
oleksiikrutko <krutko.av@gmail.com>
2021-08-11 09:02:10 +0300

    add model User

---
+++ src/main/java/ua/goit/model/User.java
@@ -0,0 +1,41 @@
+package ua.goit.model;
+
+public class User implements BaseEntity<Integer>{
+    private Integer id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private Integer userStatus;
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", userStatus=" + userStatus +
+                '}';
+    }
+    /*{
+  "id": 0,
+  "username": "string",
+  "firstName": "string",
+  "lastName": "string",
+  "email": "string",
+  "password": "string",
+  "phone": "string",
+  "userStatus": 0
+}*/
+}
